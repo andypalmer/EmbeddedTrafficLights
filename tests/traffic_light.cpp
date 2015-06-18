3a8010f7aeacd42e35f031d8978a663009e1395f
Andy Palmer <andy@andypalmer.com>
2015-06-18 11:59:05 +0100

    Change to designated initializer

--- tests/traffic_light.cpp
+++ tests/traffic_light.cpp
@@ -9,8 +9,14 @@ TEST_GROUP(TrafficLight) {
 };
 
 TEST(TrafficLight, is_on_stop_by_default) {
-  TrafficLightLamps lamps = {EmulatedLed_Create(), 0, 0};
+  TrafficLightLamps lamps = {
+    .red = EmulatedLed_Create(),
+    .amber = EmulatedLed_Create(),
+    .green = EmulatedLed_Create()};
   TrafficLight traffic_light = TrafficLight_Create(lamps);
   LONGS_EQUAL(TrafficLight_State(traffic_light), Traffic_STOP);
+   //LONGS_EQUAL(LedDriver_State(red), Led_ON);
+   //LONGS_EQUAL(LedDriver_State(amber), Led_OFF);
+   //LONGS_EQUAL(LedDriver_State(green), Led_OFF);
   TrafficLight_Destroy(traffic_light);
 }
