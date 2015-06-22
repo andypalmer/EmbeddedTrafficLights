e9933e499091bedef7307df24998681cb7f77544
Andy Palmer <andy@andypalmer.com>
2015-06-22 08:48:56 +0100

    traffic lights work with the arduino

--- _platforms/arduino/test.c
+++ _platforms/arduino/test.c
@@ -1,25 +1,26 @@
 #include "LedDriver.h"
 #include "ArduinoLed.h"
+#include "TrafficLight.h"
 #include "WProgram.h"
 
-static LedDriver red;
-static LedDriver green;
+static LedDriver red,amber,green;
+static TrafficLight traffic_light;
 
 void setup() {}
 void loop() {
-  delay(1000);
-  LedDriver_TurnOff(green);
-  delay(1000);
-  LedDriver_TurnOn(green);
-  delay(1000);
-  LedDriver_TurnOff(red);
+  delay(2000);
+  TrafficLight_NextState(traffic_light);
+
 }
 
 int main() {
   init();
-  setup();
   red = ArduinoLed_Create(13);
+  amber = ArduinoLed_Create(12);
   green = ArduinoLed_Create(11);
+  TrafficLightLamps lamps = {.red = red, .amber = amber, .green = green};
+  traffic_light = TrafficLight_Create(lamps);
+
   while(1)
     loop();
 }
